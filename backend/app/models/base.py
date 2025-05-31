70d06bfbfe032c09940286e4698a521175ce7903
m.ilchenko <m.ilchenko@mytechhelp.ru>
2025-06-01 00:34:24 +0300

    add refresh

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,9 +1,12 @@
 import uuid
+from datetime import datetime
 
 from core.database import Base
 from sqlalchemy import ForeignKey, String, Text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.functions import func
+from sqlalchemy.sql.sqltypes import Boolean, DateTime
 
 
 class UserModel(Base):
@@ -17,6 +20,10 @@ class UserModel(Base):
 
     courses: Mapped[list["CourseModel"]] = relationship(back_populates="user")
     preferences: Mapped[list["PreferenceModel"]] = relationship(back_populates="user")
+    refresh_tokens: Mapped[list["RefreshTokenModel"]] = relationship(
+        back_populates="user",
+        cascade="all, delete-orphan",
+    )
 
 
 class PreferenceModel(Base):
@@ -55,4 +62,27 @@ class SessionModel(Base):
     courses: Mapped[list["CourseModel"]] = relationship(back_populates="session")
     preferences: Mapped[list["PreferenceModel"]] = relationship(
         back_populates="session"
+    )
+
+
+class RefreshTokenModel(Base):
+    __tablename__ = "refresh_tokens"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    token: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    revoked: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+
+    user: Mapped["UserModel"] = relationship(
+        back_populates="refresh_tokens", lazy="selectin"
     )
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,10 +1,12 @@
 import uuid
 
+from uuid import UUID
+
 from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class UserBase(BaseModel):
-    id: int
+    id: UUID
     email: EmailStr
     username: str
     model_config = ConfigDict(from_attributes=True)
@@ -12,10 +14,16 @@ class UserBase(BaseModel):
 
 class Token(BaseModel):
     access_token: str
-    token_type: str = "bearer"
+    token_type: str = "Bearer"
+    refresh_token: str
+    model_config = ConfigDict(populate_by_name=True, from_attributes=True)
 
 
 class UserRegister(BaseModel):
     username: EmailStr
     password: str
     session_id: uuid.UUID | None = None
+
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
